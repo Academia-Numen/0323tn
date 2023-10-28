f8fe77d603381961211afe96371f45f8ac1d36d2
Academia-Numen <81168138+Academia-Numen@users.noreply.github.com>
2023-10-28 19:06:20 -0300

    Update api.js

--- router/api.js
+++ router/api.js
@@ -4,12 +4,11 @@ const apiController = require('../controller/apiController')
 const {validarID}= require('../middlewares/validarID')
 const checks = require('../middlewares/checks')
 const {validarChecks}= require('../middlewares/validarChecks')
-const validarToken = require('../middlewares/validarToken')
 
 // metodo http - URN o expresion - middlewares - middlewares - middlewares - middlewares - middlewares - callback
 
-router.get('/list', validarToken,apiController.apiList)
-router.post('/crear',validarToken, checks,validarChecks,apiController.apiCrearProducto)
+router.get('/list', apiController.apiList)
+router.post('/crear', checks,validarChecks,apiController.apiCrearProducto)
 router.put('/editar/:id',validarID,checks,validarChecks ,apiController.apiPut)
 router.delete('/eliminar/:id', validarID,apiController.apiDelete)
 
